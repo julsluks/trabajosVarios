7eb6427fd2e52565b9b41c44d77de27b43d1d94a
julsluks <a22betvilver@inspedralbes.cat>
2024-02-22 14:32:44 +0100

    add tests

--- front/vue-project/src/function/__tests__/arrayFunctions.spec.js
+++ front/vue-project/src/function/__tests__/arrayFunctions.spec.js
@@ -1,5 +1,9 @@
 import { describe, expect, test } from 'vitest'
 import { arrayLength } from '../arrayFunctions'
+import { arrayPush } from '../arrayFunctions'
+import { arrayPop } from '../arrayFunctions'
+import { arrayShift } from '../arrayFunctions'
+import { arrayUnshift } from '../arrayFunctions'
 
 describe('array lenght function', () => {
 
@@ -13,5 +17,65 @@ describe('array lenght function', () => {
 
     test('array 2 elementos', () => {
         expect(arrayLength([1, 2])).toBe(2);
+    })
+})
+
+describe('array push function', () => {
+
+    test('array vacio', () => {
+        expect(arrayPush([], 1)).toEqual([1]);
+    })
+
+    test('array un elemento', () => {
+        expect(arrayPush([1], 2)).toEqual([1, 2]);
+    })
+
+    test('array 2 elementos', () => {
+        expect(arrayPush([1, 2], 3)).toEqual([1, 2, 3]);
+    })
+})
+
+describe('array pop function', () => {
+
+    test('array vacio', () => {
+        expect(arrayPop([])).toEqual([]);
+    })
+
+    test('array un elemento', () => {
+        expect(arrayPop([1])).toEqual([]);
+    })
+
+    test('array 2 elementos', () => {
+        expect(arrayPop([1, 2])).toEqual([1]);
+    })
+})
+
+describe('array shift function', () => {
+
+    test('array vacio', () => {
+        expect(arrayShift([])).toEqual([]);
+    })
+
+    test('array un elemento', () => {
+        expect(arrayShift([1])).toEqual([]);
+    })
+
+    test('array 2 elementos', () => {
+        expect(arrayShift([1, 2])).toEqual([2]);
+    })
+})
+
+describe('array unshift function', () => {
+
+    test('array vacio', () => {
+        expect(arrayUnshift([], 1)).toEqual([1]);
+    })
+
+    test('array un elemento', () => {
+        expect(arrayUnshift([1], 2)).toEqual([2, 1]);
+    })
+
+    test('array 2 elementos', () => {
+        expect(arrayUnshift([1, 2], 3)).toEqual([3, 1, 2]);
     })
 })
--- front/vue-project/src/function/arrayFunctions.js
+++ front/vue-project/src/function/arrayFunctions.js
@@ -8,3 +8,44 @@ export function arrayLength(array) {
     }
     return i;
 }
+
+export function arrayPush(array, value) {
+    let i = 0;
+    while (array[i] !== undefined) {
+        i++;
+    }
+    array[i] = value;
+    return array;
+}
+
+export function arrayPop(array) {
+    if (array.length === 0) {
+        return array;
+    }
+
+    const newArray = [];
+    for (let i = 0; i < array.length - 1; i++) {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+export function arrayShift(array) {
+    if (array.length === 0) {
+        return array;
+    }
+
+    const newArray = [];
+    for (let i = 1; i < array.length; i++) {
+        newArray[i - 1] = array[i];
+    }
+    return newArray;
+}
+
+export function arrayUnshift(array, value) {
+    const newArray = [value];
+    for (let i = 0; i < array.length; i++) {
+        newArray[i + 1] = array[i];
+    }
+    return newArray;
+} 
